614e29154bc457e1b95265c4b139173e842a4afd
Brennan M <brennanmcvicar@gmail.com>
2018-02-06 06:12:33 -0500

    Persist state via local storage

--- src/reducers/temperatureSystem.reducer.js
+++ src/reducers/temperatureSystem.reducer.js
@@ -2,31 +2,12 @@ const TOGGLE_TEMPERATURE_SYSTEM = 'TOGGLE_TEMPERATURE_SYSTEM';
 
 const toggleTemperatureSystem = () => ({type: TOGGLE_TEMPERATURE_SYSTEM});
 
-const kelvinToCelsiusRounded = K => round(kelvinToCelsius(K));
-const kelvinToFarenheitRounded = K => round(kelvinToFarenheit(K));
-const kelvinToFarenheit = (temp) => temp * (9 / 5) - 459.67;
-const kelvinToCelsius = (temp) => temp - 273.15;
-
-const round = (num, places = 0) => {
-  const mult = Math.pow(10, places);
-  return Math.round(num * mult) / mult;
-};
-
-const defaultState = {
-	convertFromK: kelvinToFarenheitRounded,
-	abbr: 'F'
-};
+const defaultState = 'F';
 
 const reducer = (state = defaultState, action) => {
 	switch(action.type) {
 		case TOGGLE_TEMPERATURE_SYSTEM:
-			return {
-				...state, 
-				abbr: state.abbr === 'F' ? 'C' : 'F',
-				convertFromK: state.abbr === 'F' ? 
-					kelvinToCelsiusRounded : 
-					kelvinToFarenheitRounded
-			};
+			return state.abbr === 'F' ? 'C' : 'F';
 		default:
 			return state;
 	}
