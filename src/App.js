bc3451b2250ca6c7f6e478a587d6a66355bd8367
Brennan M <brennanmcvicar@gmail.com>
2019-02-14 15:27:22 -0500

    Remove react-router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import FullForecast from './components/FullForecast.js';
 import DetermineLocation from './components/DetermineLocation.js';
-
-const Home = () => (
-	<DetermineLocation>
-		<FullForecast />
-	</DetermineLocation>
-);
 
 class App extends Component {
 
 	render() {
 		return (
-			<Router basename={process.env.NODE_ENV === 'production' ? '/fcc-weather-app' : '/'}>
-				<Route path="/" exact component={Home} />
-			</Router>
+			<DetermineLocation>
+				<FullForecast />
+			</DetermineLocation>
 		);
 	}
 }
